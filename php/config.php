3c721fff704b19f1341b951f28f3e770320d13eb
Nat-ck <natanck56@gmail.com>
2025-10-21 04:29:30 -0700

    version 2

--- php/config.php
+++ php/config.php
@@ -6,7 +6,7 @@ $dbName = "natix";
 
 $dbConnection = new mysqli($serverHostname, $dbusername, $dbPassword, $dbName);
 if($dbConnection->connect_error){
-   die("connection failed" . $dbConnection->connect_error);
+die("connection failed" . $dbConnection->connect_error);
 }
 else{
     echo("connnected successfully!");
